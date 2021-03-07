43060bb2ea3a054acf829f42ea4fc1c7807ecf64
岩瀬旭 <a-iwase@shikaku-square.com>
2021-03-08 07:55:16 +0900

    Ref indent

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -57,13 +57,14 @@ class RoomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_room
-      @room = Room.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def room_params
-      params.require(:room).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
